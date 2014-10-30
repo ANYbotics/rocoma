15b8699f5859d5c3d078f524ff63457b38a5b692
gehrinch <gehrinch@ethz.ch>
2014-10-30 18:56:50 +0100

    Added robot model.

--- include/locomotion_controller/LocomotionController.hpp
+++ include/locomotion_controller/LocomotionController.hpp
@@ -13,12 +13,26 @@
 #include <sensor_msgs/Joy.h>
 #include <starleth_msgs/SeActuatorCommands.h>
 
+#include "RobotModel.hpp"
+
+#include <kindr/rotations/RotationEigen.hpp>
+#include <kindr/rotations/RotationDiffEigen.hpp>
+#include <kindr/phys_quant/PhysicalQuantitiesEigen.hpp>
+
 #include <memory>
 
 namespace locomotion_controller {
 
 class LocomotionController
 {
+ public:
+  typedef kindr::rotations::eigen_impl::RotationQuaternionPD RotationQuaternion;
+  typedef kindr::rotations::eigen_impl::EulerAnglesZyxPD EulerAnglesZyx;
+  typedef kindr::rotations::eigen_impl::LocalAngularVelocityPD LocalAngularVelocity;
+  typedef kindr::rotations::eigen_impl::AngleAxisPD AngleAxis;
+  typedef kindr::phys_quant::eigen_impl::Position3D Position;
+  typedef kindr::phys_quant::eigen_impl::Velocity3D LinearVelocity;
+  typedef kindr::phys_quant::eigen_impl::VectorTypeless3D Vector;
  public:
   LocomotionController(ros::NodeHandle& nodeHandle);
   virtual ~LocomotionController();
@@ -31,12 +45,16 @@ class LocomotionController
   void publish();
   void robotStateCallback(const starleth_msgs::RobotState::ConstPtr& msg);
   void joystickCallback(const sensor_msgs::Joy::ConstPtr& msg);
+  void setRobotModelParameters();
+  void updateRobotModelFromRobotState(const starleth_msgs::RobotState::ConstPtr& robotState);
  private:
   ros::NodeHandle& nodeHandle_;
   ros::Subscriber robotStateSubscriber_;
   ros::Subscriber joystickSubscriber_;
   ros::Publisher jointCommandsPublisher_;
   starleth_msgs::SeActuatorCommandsPtr jointCommands_;
+  double timeStep_;
+  std::shared_ptr<robotModel::RobotModel> robotModel_;
 };
 
 } /* namespace locomotion_controller */
--- src/LocomotionController.cpp
+++ src/LocomotionController.cpp
@@ -10,7 +10,9 @@
 namespace locomotion_controller {
 
 LocomotionController::LocomotionController(ros::NodeHandle& nodeHandle):
-    nodeHandle_(nodeHandle)
+    nodeHandle_(nodeHandle),
+    timeStep_(0.0025),
+    robotModel_()
 {
 
 }
@@ -33,6 +35,25 @@ bool LocomotionController::initialize() {
   jointCommandsPublisher_ = nodeHandle_.advertise<starleth_msgs::SeActuatorCommands>(jointCommandsTopicName, 1);
 
   jointCommands_.reset(new starleth_msgs::SeActuatorCommands);
+  for (int i=0; i<jointCommands_->commands.size(); i++) {
+    jointCommands_->commands[i].mode =  jointCommands_->commands[i].MODE_MOTOR_VELOCITY;
+    jointCommands_->commands[i].motorVelocity = 0.0;
+  }
+
+  robotModel_.reset(new robotModel::RobotModel(timeStep_));
+  setRobotModelParameters();
+  robotModel_->setIsRealRobot(false);
+  /* Select estimator: */
+  robotModel_->est().setActualEstimator(robotModel::PE_SL); // feed through of simulated states
+  //  robotModel.est().setActualEstimator(robotModel::PE_ObservabilityConstrainedEKF); // activate this estimator
+  //  robotModel.est().setActualEstimator(robotModel::PE_LSE); // not used
+
+  /* activate sensor noise */
+  robotModel_->sensors().setIsAddingSensorNoise(false);
+  robotModel_->act().setUseLimitsInSimFlag(false); // do not change! see jointController for activating limits
+  /* initialize robot model */
+  robotModel_->init();
+
   return true;
 }
 
@@ -48,6 +69,9 @@ void LocomotionController::publish()  {
   }
 }
 void LocomotionController::robotStateCallback(const starleth_msgs::RobotState::ConstPtr& msg) {
+  updateRobotModelFromRobotState(msg);
+
+
   ros::Time stamp = ros::Time::now();
   for (int i=0; i<jointCommands_->commands.size(); i++) {
     jointCommands_->commands[i].header.stamp = stamp;
@@ -56,7 +80,137 @@ void LocomotionController::robotStateCallback(const starleth_msgs::RobotState::C
 }
 
 void LocomotionController::joystickCallback(const sensor_msgs::Joy::ConstPtr& msg) {
+//  robotModel_->sensors().getJoystick()->
+}
+
+void LocomotionController::setRobotModelParameters() {
+#ifdef I
+#undef I
+#endif
+  robotModel_->params().gravity_ = 9.81;
+
+  /* main body */
+  robotModel_->params().mainbody_.m = 17.1;
+  robotModel_->params().mainbody_.I(0,0) = 0.226;
+  robotModel_->params().mainbody_.I(1,1) = 0.393;
+  robotModel_->params().mainbody_.I(2,2) = 0.553;
+  robotModel_->params().mainbody_.s = 0.0;
+  robotModel_->params().mainbody_.b2hx = 0.505/2.0;
+  robotModel_->params().mainbody_.b2hy = 0.37/2.0;
+  robotModel_->params().mainbody_.l = 2.0 * robotModel_->params().mainbody_.b2hx ;
+  robotModel_->params().mainbody_.b = 2.0 * robotModel_->params().mainbody_.b2hy;
+
+
+  /* hip */
+  robotModel_->params().hip_.m = 1.8;
+  robotModel_->params().hip_.I(0,0) = 0.009275;
+  robotModel_->params().hip_.I(1,1) = 0.003035;
+  robotModel_->params().hip_.I(2,2) = 0.007210;
+  robotModel_->params().hip_.s =  -0.1480;
+  robotModel_->params().hip_.l =  -0.2000;
+
+  /* thigh */
+  robotModel_->params().thigh_.m =  0.3100;
+  robotModel_->params().thigh_.I(0,0) = 0.00148976;
+  robotModel_->params().thigh_.I(1,1) = 0.00142976;
+  robotModel_->params().thigh_.I(2,2) =  0.00021500;
+  robotModel_->params().thigh_.s = -0.1480;
+  robotModel_->params().thigh_.l = -0.2000;
+//   printf("thigh length: %lf", robotModel_->params().thigh_.l);
+
+  /* shank */
+  robotModel_->params().shank_.m =  0.3200;
+  robotModel_->params().shank_.I(0,0) =  0.00112108;
+  robotModel_->params().shank_.I(1,1) =  0.00124208;
+  robotModel_->params().shank_.I(2,2) =  0.00020000;
+  robotModel_->params().shank_.s =  -0.0840;
+  robotModel_->params().shank_.l = -0.2350;
+//   printf("shank length: %lf", robotModel_->params().shank_.l);
+  robotModel_->params().computeTotalMass();
 
 }
 
+void LocomotionController::updateRobotModelFromRobotState(const starleth_msgs::RobotState::ConstPtr& robotState) {
+
+  namespace rot = kindr::rotations::eigen_impl;
+
+  static robotModel::VectorQb Qb = robotModel::VectorQb::Zero();
+  static robotModel::VectorQb dQb = robotModel::VectorQb::Zero();
+  static robotModel::VectorAct jointTorques;
+  static robotModel::VectorQj jointPositions;
+  static robotModel::VectorQj jointVelocities;
+
+  Qb(0) = robotState->pose.position.x;
+  Qb(1) = robotState->pose.position.y;
+  Qb(2) = robotState->pose.position.z;
+
+
+  RotationQuaternion orientationWorldToBase(robotState->pose.orientation.w,
+                                            robotState->pose.orientation.x,
+                                            robotState->pose.orientation.y,
+                                            robotState->pose.orientation.z);
+
+  Qb.segment<3>(3) = rot::EulerAnglesXyzPD(orientationWorldToBase).vector();
+
+
+  dQb(0) = robotState->twist.linear.x;
+  dQb(1) = robotState->twist.linear.y;
+  dQb(2) = robotState->twist.linear.z;
+
+//  static mule::Vector3d globalAngularVelocity;
+//  simulationManager.getRobotBaseAngularVelocityInWorldCoordinates(
+//      globalAngularVelocity, idStarleth);
+
+
+   LocalAngularVelocity angularVelocityKindr(robotState->twist.angular.x,
+                                             robotState->twist.angular.y,
+                                             robotState->twist.angular.z);
+    const Eigen::Vector3d drpy = rot::EulerAnglesXyzDiffPD(
+        rot::EulerAnglesXyzPD(orientationWorldToBase), angularVelocityKindr)
+        .toImplementation();
+    dQb.tail(3) = drpy;
+
+    Eigen::Vector3d globalAngularVelocity = orientationWorldToBase.inverseRotate(angularVelocityKindr.toImplementation());
+
+
+  /* set contacts */
+  static Eigen::Vector4i contactFlags;
+  static Eigen::Vector3d force;
+  static Eigen::Vector3d normal;
+  bool isClosed;
+  for (int iFoot = 0; iFoot < 4; iFoot++) {
+    contactFlags(iFoot) =  (robotState->contacts[iFoot].state == robotState->contacts[iFoot].STATE_CLOSED) ? 1: 0;
+
+    normal.x() = robotState->contacts[iFoot].normal.x;
+    normal.y() = robotState->contacts[iFoot].normal.y;
+    normal.z() = robotState->contacts[iFoot].normal.z;
+
+    force.x() = robotState->contacts[iFoot].wrench.force.x;
+    force.y() = robotState->contacts[iFoot].wrench.force.y;
+    force.z() = robotState->contacts[iFoot].wrench.force.z;
+
+    robotModel_->sensors().setContactForceCSw(iFoot, force);
+    robotModel_->sensors().setContactNormalCSw(iFoot, normal);
+
+  }
+  for (int i = 0; i < jointPositions.size(); i++) {
+    jointTorques(i) = robotState->joints.effort[i];
+    jointPositions(i) = robotState->joints.position[i];
+    jointVelocities(i) = robotState->joints.velocity[i];
+  }
+
+  /* update robot model */
+  robotModel_->sensors().setJointTorques(jointTorques);
+  robotModel_->sensors().setJointPos(jointPositions);
+  robotModel_->sensors().setJointVel(jointVelocities);
+//  robotModel_->sensors().setJointAcc(jointAccelerations);
+  robotModel_->sensors().getSimMainBodyPose()->setQb(Qb);
+  robotModel_->sensors().getSimMainBodyPose()->setdQb(dQb);
+//  robotModel.sensors().getSimMainBodyPose()->setddQb(ddQb);
+  robotModel_->sensors().getSimMainBodyPose()->setOmega(globalAngularVelocity);
+
+  robotModel_->sensors().updateSimulatedIMU();
+  robotModel_->sensors().setContactFlags(contactFlags);
+
+}
 } /* namespace locomotion_controller */
